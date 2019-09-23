8411337ea357abf5e0dca0dfd20d0c35c4615169
xush <xush@esensoft.com>
2019-09-23 18:00:38 +0800

    ISSUE:[文件数据源]分页条添加，模态框初步使用。

--- src/main/resources/META-IN/resources/xfly/main/datasource/js/filelist.js
+++ src/main/resources/META-IN/resources/xfly/main/datasource/js/filelist.js
@@ -10,6 +10,7 @@ define(["/third/echarts.min.js"], function(echarts) {
 		this.doc = this.wnd.document;
 		this.container = options.container;
 		this.initPage(); // 初始化界面
+		this.initRightPanel();
 	}
     
 	/**
@@ -19,6 +20,8 @@ define(["/third/echarts.min.js"], function(echarts) {
 		this.wnd = null;
 		this.doc = null;
 		this.container = null;
+		this.toolbar = null;
+		this.list = null;
 	}
 	
 	
@@ -34,17 +37,21 @@ define(["/third/echarts.min.js"], function(echarts) {
 	 */
 	FileList.prototype.initUI = function() {
 		var htmlStr = [];
-		htmlStr.push('<div class="xhui-layout-row-1 xhui-layout-row-first xhui-layout-nav">');
+		htmlStr.push('<div id="toolbar" class="xhui-layout-row-1 xhui-layout-row-first xhui-layout-nav">');
 		htmlStr.push('</div>');
-		htmlStr.push('<div class="xhui-layout-row-3 xhui-layout-row-offset-1 xhui-layout-row-offsetbottom-1 ">');	
+		htmlStr.push('<div class="xhui-layout-row-3 xhui-layout-row-offset-1 xhui-layout-row-last xhui-layout-row-offsetbottom-2list ">');	
 		htmlStr.push('	<table id="filelist" class="table table-striped "></table>');
 		htmlStr.push('</div>');
-		htmlStr.push('<div class="xhui-layout-row-1 xhui-layout-row-last xhui-align-center xhui-padding-left-20 xhui-padding-right-20 xhui-padding-top-10 xhui-padding-bottom-10" >分页条保留地</div>');
 		this.container.innerHTML = htmlStr.join("");
+
+		this.toolbar = this.doc.getElementById("toolbar");
 		this.list = $('#filelist');
 		this.list.bootstrapTable({
 			striped: true, //行之间间隔色
 			cache: false,
+			pagination: true, 
+			pageList:[10,50,100],
+			pageSize: 10,                     //每页的记录行数（*）
 			columns: [
 				{
 					checkbox: true,  
@@ -90,7 +97,42 @@ define(["/third/echarts.min.js"], function(echarts) {
 	}
 	
 	FileList.prototype.initRightPanel = function() {
+		var htmlStr = [];
+		htmlStr.push('<div class="btn-group">');
+		htmlStr.push('	<div class="btn-group">');
+		htmlStr.push('		<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">新建');
+		htmlStr.push('     		<span class="caret"></span>');
+		htmlStr.push('		 </button>');
+		htmlStr.push('		 <ul class="dropdown-menu">');
+		htmlStr.push('     		<li><button class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">Excel</button></li>');
+		htmlStr.push('     		<li><a href="#">其他</a></li>');
+		htmlStr.push(' 		</ul>');
+		htmlStr.push('	</div>');
+		htmlStr.push('	<button type="button" class="btn btn-default">删除</button>');
+		htmlStr.push('	<button type="button" class="btn btn-default">刷新</button>');
+		htmlStr.push('</div>');
+		this.toolbar.innerHTML = htmlStr.join("");
 		
+		var model = this.doc.createElement("div");
+		var modelHtml = [];
+		modelHtml.push('<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">');
+		modelHtml.push('<div class="modal-dialog">');
+		modelHtml.push('    <div class="modal-content">');
+		modelHtml.push('        <div class="modal-header">');
+		modelHtml.push('             <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>');
+		modelHtml.push('            <h4 class="modal-title" id="myModalLabel">模态框（Modal）标题</h4>');
+		modelHtml.push('        </div>');
+		modelHtml.push('        <div class="modal-body">在这里添加一些文本</div>');
+		modelHtml.push('        <div class="modal-footer">');
+		modelHtml.push('            <button type="button" class="btn btn-default" data-dismiss="modal">关闭</button>');
+		modelHtml.push('            <button type="button" class="btn btn-primary">提交更改</button>');
+		modelHtml.push('        </div>');
+		modelHtml.push('    </div><!-- /.modal-content -->');
+		modelHtml.push('</div><!-- /.modal -->');
+		modelHtml.push('</div>');
+	    model.innerHTML = modelHtml.join("");
+	    debugger
+	    this.container.appendChild(model);
 	}
 
 	return {
