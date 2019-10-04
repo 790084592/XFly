f2dd7364c329d47d3fee52ffc3e0387fcad21f46
gelh <gelh@esensoft.com>
2019-10-04 10:33:09 +0800

    修改依赖

--- src/main/java/com/xush/demo/controller/TableCreator.java
+++ src/main/java/com/xush/demo/controller/TableCreator.java
@@ -4,6 +4,7 @@ import java.text.ParseException;
 
 import javax.annotation.PostConstruct;
 
+import cn.hutool.db.Db;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
