ddd1e5671d1b273082fb9b91a1ed166bb4e2fe2b
xush <xush@esensoft.com>
2019-10-09 20:02:18 +0800

    代码优化

--- src/main/java/com/xush/demo/controller/TableCreator.java
+++ src/main/java/com/xush/demo/controller/TableCreator.java
@@ -1,47 +1,145 @@
 package com.xush.demo.controller;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.text.ParseException;
+import java.util.ArrayList;
 
 import javax.annotation.PostConstruct;
 
-import cn.hutool.db.Db;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
 import com.xush.demo.util.JdbcFunc;
 
 /**
  * 项目系统表建立与检测
+ * 
  * @author xush
- * @since  2019年9月26日
+ * @since 2019年9月26日
  */
 @Component
 public class TableCreator {
-	
+
 	@Autowired
-  private JdbcTemplate jdbcTemplate;
+	private JdbcTemplate jdbcTemplate;
+
+	@Value("${xfly.datasource.type}")
+	private String dataBaseType;
+
+	public static final String MYSQL = "MYSQL";
+
+	public static final String ORACLE = "ORACLE";
 
 	/**
-	    *       配置系统表
-	 * @throws ParseException 
+	 * 配置系统表
+	 * 
+	 * @throws ParseException
 	 */
 	@PostConstruct
 	public void init() throws ParseException {
-		boolean exist = JdbcFunc.checkTableExist("XFLY_FILEDATASOURCE", jdbcTemplate);
-		if(!exist) {
-			StringBuffer sf = new StringBuffer();
-			sf.append("create table XFLY_FILEDATASOURCE(");
-			sf.append("		id varchar(32) primary key, ");
-			sf.append("		name varchar(64) not null, ");
-			sf.append("		path varchar(128) not null, ");
-			sf.append("		type number(32) not null, ");
-			sf.append("		createdate varchar(32) not null, ");
-			sf.append("		creator varchar(32) not null ");
-			sf.append(")");
-			JdbcFunc.createTable(sf.toString(), jdbcTemplate);
+		System.out.println("   = " + dataBaseType);
+		boolean exist = checkTableExist("XFLY_FILEDATASOURCE");
+		if (!exist) {
+			createDataSourceTable();
 		}
+	}
 
+	/**
+	 * 创建文件数据源数据库表
+	 */
+	public void createDataSourceTable() {
+		String sql = "";
+		switch (dataBaseType) {
+		case MYSQL:
+			sql = getCreateDataSourceSql2Oracle();
+			break;
+		case ORACLE:
+			sql = getCreateDataSourceSql2MySql();
+			break;
+		}
+		jdbcTemplate.execute(sql);
+	}
+
+	/**
+	 * 获取sql语句
+	 * 
+	 * @return
+	 */
+	public String getCreateDataSourceSql2MySql() {
+		StringBuffer sf = new StringBuffer();
+		sf.append("create table XFLY_FILEDATASOURCE(");
+		sf.append("		id varchar(32) primary key, ");
+		sf.append("		name varchar(64) not null, ");
+		sf.append("		path varchar(128) not null, ");
+		sf.append("		type int(32) not null, ");
+		sf.append("		createdate varchar(32) not null, ");
+		sf.append("		creator varchar(32) not null ");
+		sf.append(")");
+		return sf.toString();
+	}
+
+	/**
+	 * 获取sql语句
+	 * 
+	 * @return
+	 */
+	public String getCreateDataSourceSql2Oracle() {
+		StringBuffer sf = new StringBuffer();
+		sf.append("create table XFLY_FILEDATASOURCE(");
+		sf.append("		id varchar(32) primary key, ");
+		sf.append("		name varchar(64) not null, ");
+		sf.append("		path varchar(128) not null, ");
+		sf.append("		type int(32) not null, ");
+		sf.append("		createdate varchar(32) not null, ");
+		sf.append("		creator varchar(32) not null ");
+		sf.append(")");
+		return sf.toString();
+	}
+
+	/**
+	 * 检查数据库表是否存在
+	 * 
+	 * @param tableName
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public boolean checkTableExist(String tableName) {
+		String sql = "";
+		switch (dataBaseType) {
+		case MYSQL:
+			sql = getCheckTableExistSql2MySql(tableName);
+			break;
+		case ORACLE:
+			sql = getCheckTableExistSql2Oracle(tableName);
+			break;
+		}
+		ArrayList<Integer> list = (ArrayList<Integer>) jdbcTemplate.query(sql, new RowMapper() {
+			public Integer mapRow(ResultSet rs, int rowNum) throws SQLException {
+				int answer = rs.getInt("answer");
+				return answer;
+			}
+		});
+		if (list.size() > 0) {
+			return list.get(0) > 0;
+		}
+		return false;
+	}
+
+	public String getCheckTableExistSql2MySql(String tableName) {
+		String sql = String.format(
+				"SELECT count(*) as answer FROM information_schema.TABLES WHERE table_name =upper('%s');", tableName);
+		return sql;
+	}
+
+	public String getCheckTableExistSql2Oracle(String tableName) {
+		String sql = String.format(
+				"select count(*) as answer from INFORMATION_SCHEMA.TABLES whereTABLE_SCHEMA='%s' and TABLE_NAME=upper('%s')",
+				"test", tableName);
+		return sql;
 	}
 
 }
--- src/main/java/com/xush/demo/util/JdbcFunc.java
+++ src/main/java/com/xush/demo/util/JdbcFunc.java
@@ -17,12 +17,14 @@ public class JdbcFunc {
 	 * @return
 	 */
 	public static boolean checkTableExist(String tableName, JdbcTemplate jdbcTemplate) {
-		String sql = String.format("select count(*) as answer from INFORMATION_SCHEMA.TABLES whereTABLE_SCHEMA='%s' and TABLE_NAME='%s' ;", "test",tableName );
+		String sql = String.format("select count(*) as answer from INFORMATION_SCHEMA.TABLES whereTABLE_SCHEMA='%s' and TABLE_NAME='%s' ", "test",tableName );
 //		String sql = "select count(*) as answer from user_tables where table_name =upper('" + tableName + "')";
+		String sql2 = "SELECT count(*) as answer FROM information_schema.TABLES WHERE table_name =upper('" + tableName+ "');";
+
 		if(jdbcTemplate == null) {
 			System.out.println("----jdbc null");
 		}
-		ArrayList<Integer> list = (ArrayList<Integer>) jdbcTemplate.query(sql, new RowMapper() {
+		ArrayList<Integer> list = (ArrayList<Integer>) jdbcTemplate.query(sql2, new RowMapper() {
 			public Integer mapRow(ResultSet rs, int rowNum) throws SQLException {
 				int answer = rs.getInt("answer");
 				return answer;
